7e546d5ebf85ca86dea00b6679fc7d75e04f28d3
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-07-24 20:21:53 -0500

    evento TRANSITIONEND

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.getElementById('animate').addEventListener('click', e=>{
-    document.body.classList.toggle('animate')
-    e.target.textContent=='animar'
-    ?e.target.textContent='detener'
-    :e.target.textContent='animar'
+document.getElementById('boton').addEventListener('click',(e) =>{
+    document.body.classList.add('animar')
+
+    document.getElementById('pelota').addEventListener('transitionend',e =>{
+       // console.log(e)
+
+        if (e.propertyName==='top') {
+            document.body.classList.remove('animar')
+            console.log(e)
+            
+        }
+    })
+
+
 })
+
+
