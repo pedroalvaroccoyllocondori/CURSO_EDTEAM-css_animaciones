348404e74ba0dd3952038961a4567d431be087d5
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-07-24 21:17:55 -0500

    disparadores de transciones speudoclases (:active :target)

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.getElementById('boton').addEventListener('click',(e) =>{
-    document.body.classList.add('animar')
+// document.getElementById('boton').addEventListener('click',(e) =>{
+//     document.body.classList.add('animar')
 
-    document.getElementById('pelota').addEventListener('transitionend',e =>{
-       // console.log(e)
+//     document.getElementById('pelota').addEventListener('transitionend',e =>{
+//        // console.log(e)
 
-        if (e.propertyName==='top') {
-            document.body.classList.remove('animar')
-            console.log(e)
+//         if (e.propertyName==='top') {
+//             document.body.classList.remove('animar')
+//             console.log(e)
             
-        }
-    })
+//         }
+//     })
 
 
-})
+// })
 
 
